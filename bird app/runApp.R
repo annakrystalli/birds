5de7ac98a4116f8416f147f9505a4b2d74f4fa23
annakrystalli <annakrystalli@googlemail.com>
2015-11-10 15:35:55 +0000

    Add runApp script to bird app folder version control

---
+++ bird app/runApp.R
@@ -0,0 +1,11 @@
+install.packages("shiny")
+install.packages("dplyr")
+install.packages("devtools")
+install.packages("tidyr")
+install.packages("psych")
+install.packages("shinyjs")
+
+library(devtools)
+install_github("ropensci/plotly")
+
+shiny::runApp()
